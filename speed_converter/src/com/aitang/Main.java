37f1d4b0463999ae3d884945b892120aff5abd3b
Aimin Tang <aimin.tang@gmail.com>
2019-04-01 13:44:44 -0500

    speed converter

--- speed_converter/src/com/aitang/Main.java
+++ speed_converter/src/com/aitang/Main.java
@@ -1,4 +1,9 @@
-package com.aitang;
+//import com.aitang.SpeedConverter;
+
+import com.aitang.SpeedConverter;
 
 public class Main {
+    public static void main(String[] args) {
+        SpeedConverter.printConversion(1.5);
+    }
 }
--- speed_converter/src/com/aitang/MegaBytesAndKiloBytes.java
+++ speed_converter/src/com/aitang/MegaBytesAndKiloBytes.java
@@ -1,4 +1,12 @@
-package com.aitang;
-
 public class MegaBytesAndKiloBytes {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int m = kiloBytes / 1024;
+        int k = kiloBytes % 1024;
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + m + " MB and " + k + " KB");
+        }
+    }
 }
