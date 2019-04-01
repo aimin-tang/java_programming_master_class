a788f5d274a7ef898b0350a03709294b8c0a68f3
Aimin Tang <aimin.tang@gmail.com>
2019-04-01 09:32:07 -0500

    decimal types

---
+++ decimalTypes/src/com/aitang/Main.java
@@ -0,0 +1,13 @@
+package com.aitang;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double numPounds = 200d;
+        double numKg = numPounds * 0.45359237;
+        System.out.println("weight in Kg: " + numKg);
+
+        char myChar = '\u51A9';
+        System.out.println(myChar);
+    }
+}
